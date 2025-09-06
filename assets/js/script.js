d11e1334cccb6fbaa70af290aaf26e298b383f63
moranjohn-95 <moranjohn95@gmail.com>
2025-09-06 16:20:08 +0100

    Add DOM elements for main sections/boxes

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// DOM Elements 
+
+// For Main sections/boxes
+const homeQuizBoxArea = document.getElementById("home-quiz-box-area");
+const instructionBoxArea = document.getElementById("instruction-box-area");
+const usernameInputArea = document.getElementById("username-input-area");
+const categoryOptionsArea = document.getElementById("category-options-area");
+const quizCountdownArea = document.getElementById("quiz-countdown-area");
+const questionsAnswersArea = document.getElementById("questions-answers-area");
+const homeQuizBox = document.getElementById("home-quiz-box-area");
+
+
