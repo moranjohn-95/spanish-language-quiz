3ac4172859eb9f448e68b9714878488eb3894c5f
moranjohn-95 <moranjohn95@gmail.com>
2025-09-06 17:30:25 +0100

    Add Action Button comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // DOM Elements 
 
-// For Main sections/boxes
+// Main sections/boxes
 const homeQuizBoxArea = document.getElementById("home-quiz-box-area");
 const instructionBoxArea = document.getElementById("instruction-box-area");
 const usernameInputArea = document.getElementById("username-input-area");
@@ -9,6 +9,7 @@ const quizCountdownArea = document.getElementById("quiz-countdown-area");
 const questionsAnswersArea = document.getElementById("questions-answers-area");
 const homeQuizBox = document.getElementById("home-quiz-box-area");
 
+// Action Buttons
 const showInstructionsButton = document.getElementById("show-instructions");
 const backToQuizButton = document.getElementById("back-to-quiz");
 const StartQuizButton = document.getElementById("start-quiz");
