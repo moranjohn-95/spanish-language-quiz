3bebde9a343570d7c3f6ec4a933e3377e33ae5a1
moranjohn-95 <moranjohn95@gmail.com>
2025-09-17 20:59:38 +0100

    Add link and JS code for fireworks from fireworks-js

--- assets/js/script.js
+++ assets/js/script.js
@@ -574,5 +574,24 @@ homeButton.addEventListener("click", () => {
     homeQuizBoxArea.style.display = "block";
 });
 
-
+//Fireworks effect from firework-js / https://github.com/crashmax-dev/fireworks-js
+const fireworks = new Fireworks.default(container, {
+  autoresize: true,
+  opacity: 0.5,
+  acceleration: 1.05,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 50,
+  traceLength: 3,
+  traceSpeed: 10,
+  explosion: 5,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round',
+  hue: { min: 0, max: 360 },
+  delay: { min: 30, max: 60 },
+  rocketsPoint: { min: 50, max: 50 },
+  lineWidth: { explosion: { min: 1, max: 3 }, trace: { min: 1, max: 2 } }
+});
+fireworks.start();
 
